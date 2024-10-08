3905b8550738d89cadfea5c79ef24531e1343abd
KhatoMelikidze <x.meliqidze98@gmail.com>
2024-10-08 18:23:18 +0400

    Adding Homework Week 5

---
+++ Week__5/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5
+{
+    public enum Currency
+    {
+        GEL,
+        USD,
+        EUR
+    }
+    
+}
+
---
+++ Week__5/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week__5
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Car> OwnedCars { get; set; }
+        public List<Product> PurchasedProducts { get; set; }
+
+        public Person()
+        {
+            OwnedCars = new List<Car>();
+            PurchasedProducts = new List<Product>();
+            ValidationOfTheAge();
+        }
+        public void ValidationOfTheAge()
+        {
+            if (Age < 0) Age = 0;
+            if (Age > 120) Age = 120;
+        }
+
+        public void ValidateCars()
+        {
+            foreach (var car in OwnedCars)
+            {
+                car.ValidationOfTheYear();
+            }
+        }
+
+        public void ValidationOfTheProduct()
+        {
+            foreach (var Name in PurchasedProducts)
+            {
+                Name.ValidationOfTheProduct();
+            }
+        }
+    }
+}
